8b8b984fad0f80a7c465bbecc224a15d4fb8a279
ClarityQQ <qqclarity@gmail.com>
2025-08-04 15:17:36 +0100

    Create jotfrom.js

---
+++ pages/jotfrom.js
@@ -0,0 +1,14 @@
+export default function Jotform() {
+  return (
+    <main style={{ padding: '2rem', color: '#000', backgroundColor: '#f5f0e6', minHeight: '80vh' }}>
+      <h1>Contact & Feedback Form</h1>
+
+      <iframe
+        src="https://form.jotform.com/251944073377059"
+        style={{ width: '100%', height: '700px', border: 'none', marginTop: '1rem' }}
+        title="Jotform Contact Form"
+        allowFullScreen
+      ></iframe>
+    </main>
+  );
+}
