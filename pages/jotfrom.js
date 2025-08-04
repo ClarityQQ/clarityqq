4018d7fac6e79e4fa4df8e337864402b11b11d96
ClarityQQ <qqclarity@gmail.com>
2025-08-04 15:51:35 +0100

    Update jotfrom.js

--- pages/jotfrom.js
+++ pages/jotfrom.js
@@ -1,14 +1,34 @@
+
 export default function Jotform() {
   return (
     <main style={{ padding: '2rem', color: '#000', backgroundColor: '#f5f0e6', minHeight: '80vh' }}>
       <h1>Contact & Feedback Form</h1>
-
-      <iframe
-        src="https://form.jotform.com/251944073377059"
-        style={{ width: '100%', height: '700px', border: 'none', marginTop: '1rem' }}
-        title="Jotform Contact Form"
-        allowFullScreen
-      ></iframe>
+      
+      {/* Try the direct Jotform embed first */}
+      <div style={{ marginTop: '2rem' }}>
+        <iframe
+          id="JotFormIFrame-251944073377059"
+          title="Contact & Feedback Form"
+          onLoad={() => window.parent.scrollTo(0,0)}
+          allowTransparency="true"
+          allowFullScreen="true"
+          src="https://form.jotform.com/251944073377059"
+          frameBorder="0"
+          style={{
+            minWidth: '100%',
+            maxWidth: '100%',
+            height: '700px',
+            border: 'none',
+          }}
+          scrolling="no"
+        >
+        </iframe>
+      </div>
+      
+      {/* Fallback link if iframe doesn't work */}
+      <div style={{ marginTop: '1rem', textAlign: 'center' }}>
+        <p>Form not loading? <a href="https://form.jotform.com/251944073377059" target="_blank" rel="noopener noreferrer" style={{ color: '#0066cc' }}>Open form in new tab</a></p>
+      </div>
     </main>
   );
 }
